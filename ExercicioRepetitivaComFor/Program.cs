d60ff682bceffed318da6427cb0a679d750d150a
Mariana Gonçalves <https://github.com/GoncalvesMari>
2022-01-12 22:21:46 -0300

    Continuaçao

---
+++ ExercicioRepetitivaComFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExercicioRepetitivaComFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+
+            for(int i=1; i<=N; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercicioSintaxeSwitchCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExercicioSintaxeSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+
+            switch(x)
+            {
+                case 1;
+                dia = "domingo";
+                    break;
+                case 2;
+                    dia = "terça";
+                    break;
+                case 3;
+                    dia = "quarta";
+                    break;
+                case 4;
+                    dia = "quinta";
+                    break;
+                default:
+                    dia = "valor invalido ";
+                    break;
+
+            }
+            
+
+            Console.WriteLine("Dia da Semana: " + dia);
+
+            Console.ReadLine();
+        }
+    }
+}
