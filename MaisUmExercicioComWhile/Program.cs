f5f417be1ed40a1c6c812ec6304907f2c42f6049
Mariana Gonçalves <https://github.com/GoncalvesMari>
2022-01-20 17:46:44 -0300

    Continuaçao

---
+++ MaisUmExercicioComWhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace MaisUmExercicioComWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int cont;
+
+            soma = 0.0;
+            cont = 0;
+
+            idade = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture );
+
+            while (idade >= 0)
+            {
+                soma = soma = idade;
+                cont = cont + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            if (cont == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                media = soma / cont;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
